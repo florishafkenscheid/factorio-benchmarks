b261b84295ce9563544a5eba2c9fd14e223661cd
abucnasty <abucnasty@gmail.com>
2025-07-22 04:09:18 -0400

    constant combinator generator supports characters above 24 and generates test inserter bp

--- simulators/src/blueprint/constant-combinator-offset-generator.js
+++ simulators/src/blueprint/constant-combinator-offset-generator.js
@@ -1,13 +1,20 @@
 import * as zlib from 'zlib';
 
-const offset    = 6;    // offset between inserters in ticks
-const duration  = 24;   // duration of the inserter in ticks
-const inserters = 16;   // number of inserters in the sequence
+const offset = 2;    // offset between inserters in ticks
+const duration = 1;   // duration of the inserter in ticks
+const inserters = 30;   // number of inserters in the sequence
 
 
-const total_sequence_duration = (inserters-1)*offset+duration
+const signal_names = [
+    "A", "B", "C", "D", "E", "F", "G", "H",
+    "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+    "0","1", "2", "3", "4", "5", "6", "7", "8", "9",
+]
 
-const letter_to_offset = Array.from({ length: inserters }, (_, i) => [total_sequence_duration-(offset*(inserters-i))+offset, String.fromCharCode(65 + i)]);
+
+const total_sequence_duration = (inserters - 1) * offset + duration
+
+const letter_to_offset = Array.from({ length: inserters }, (_, i) => [total_sequence_duration - (offset * (inserters - i)) + offset, signal_names[i]]);
 const filters = []
 
 letter_to_offset.forEach(([offset, letter], index) => {
@@ -58,18 +65,68 @@ const blueprint = {
     }
 }
 
-const json = JSON.stringify(blueprint)
-const blueprintString = '0' + zlib.deflateSync(json).toString('base64');
+const blueprintString = '0' + zlib.deflateSync(JSON.stringify(blueprint)).toString('base64');
 console.log(blueprintString);
 
 console.log(`total_sequence_duration = ${total_sequence_duration}`);
 
 const mod = (offset*inserters);
 console.log(`mod = ${mod}`);
-
 letter_to_offset.forEach(([offset, letter], index) => {
     const start = offset
     const end = (offset-total_sequence_duration) % mod
     console.log(`signal-${letter} = ${start}:${end}`);
 })
 
+printTestInserters()
+
+function printTestInserters() {
+
+    const createInserter = (entityNumber, x, y, signalName) => {
+        return {
+            "entity_number": entityNumber,
+            "name": "stack-inserter",
+            "position": {
+                "x": x,
+                "y": y
+            },
+            "control_behavior": {
+                "circuit_enabled": true,
+                "circuit_condition": {
+                    "first_signal": {
+                        "type": "virtual",
+                        "name": `signal-${signalName}`
+                    },
+                    "constant": 0,
+                    "comparator": ">"
+                },
+            },
+            "quality": "legendary"
+        }
+    }
+
+    const entities = letter_to_offset.map(([_, letter], index) => {
+            const x = 0.5 + (index *2);
+            return createInserter(index+1, x, 0.5, letter)
+        }
+    )
+
+    let bp = {
+        "blueprint": {
+            "icons": [
+                {
+                    "signal": {
+                        "name": "ee-infinity-chest"
+                    },
+                    "index": 1
+                }
+            ],
+            "entities": entities,
+            "wires": [],
+            "item": "blueprint",
+            "version": 562949957353472
+        }
+    }
+
+    console.log('0' + zlib.deflateSync(JSON.stringify(bp)).toString('base64'))
+}
