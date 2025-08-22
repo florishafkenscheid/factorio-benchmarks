a04ea4ee74c170bf95fcf9224df3de2352df0191
abucnasty <abucnasty@gmail.com>
2025-08-22 17:51:41 -0400

    2025 Q1 Competition: Logistics Science Submissions

---
+++ competitions/2025-Q1-Nauvis/logistics-science/benchmark.ps1
@@ -0,0 +1,19 @@
+param (
+    # number of ticks to run per save file
+    [int]$ticks = 60000,
+    # number of runs
+    [int]$runs = 3,
+    [string]$output_path = "results_verbose"
+)
+ belt benchmark maps `
+    --ticks $ticks `
+    --runs $runs `
+    --run-order random `
+    --template-path ../../../scripts/results.md.hbs `
+    --pattern "green_design_*" `
+    --output $output_path `
+    --strip-prefix "green_design_" `
+    --verbose-metrics "wholeUpdate,entityUpdate,controlBehaviorUpdate,transportLinesUpdate,electricHeatFluidCircuitUpdate"
+
+cd $output_path
+chart-gen "green_design*.csv" -w 2500 -h 800
